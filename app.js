d3bdc9b1f76b4f1b272cf880f04669b73d6cb1e1
ohjonah <ohjonah@gmail.com>
2017-05-11 15:22:25 -0700

    deleted commented out code

--- app.js
+++ app.js
@@ -31,103 +31,10 @@ for (var i = 0; i < questions.length; i++) {
 for (var i = 0; i < userAnswers.length; i++) {
   if (userAnswers[i] === answerKey[i]) {
     correctCounter++;
-    
   }
 }
 
-
-
-  // userAnswers.push(userResponse.includes(userInput));
-
-//   if (userResponse.includes(userInput) === answerKey[for loop]) {
-//
-//   }
-// }
-
-
-// var name = prompt('Is my name Andy? Y/N').toLowerCase();
-
-if (name === 'y' || name === 'yes'){
-  var correct1 = true;
-  correctAnswers.push(correct1);
-} else {
-  correct1 = false;
-}
-
-console.log('Name correct? ' + correct1);
-
-// var age = prompt('Am I turning 25 on Thursday the 11th? Y/N').toLowerCase();
-
-if (age === 'y' || age === 'yes'){
-  var correct2 = true;
-  correctAnswers.push(correct2);
-} else {
-  correct2 = false;
-}
-
-console.log('Age correct? ' + correct2);
-
-if (correct1 && correct2){
-  alert('Phew! My identity is intact! You got me pegged!');
-} else {
-  alert('You still have a bit to learn about me, my name is Andy and i\'ll soon be 25!');
-}
-
-alert('Ok, let\s make this a bit harder now.');
-
-
-// var born = prompt('Was I born in Seattle? Y/N').toLowerCase();
-
-if (born === 'n' || born === 'no'){
-  var correct3 = true;
-  correctAnswers.push(correct3);
-} else {
-  correct3 = false;
-}
-
-console.log('Born correct? ' + correct3);
-
-
-// var school = prompt('Did I go to school at Whitman? Y/N').toLowerCase();
-
-if (school === 'y' || school === 'yes'){
-  var correct4 = true;
-  correctAnswers.push(correct4);
-}else {
-  correct4 = false;
-}
-
-console.log('School correct? ' + correct4);
-
-
-// var food = prompt('Is my favorite food tacos? Y/N').toLowerCase();
-
-if (food === 'n' || food === 'no'){
-  var correct5 = true;
-  correctAnswers.push(correct5);
-}else{
-  correct5 = false;
-}
-
-console.log('Food correct? ' + correct5);
-
-if (correct3 && correct4 && correct5){
-  alert('Let\'s see how you did with those harder questions....  You were right! I was born in Minneapolis, I went to school at Whitman, and I like Whiskey just slightly more than tacos!');
-}else{
-  alert('Let\'s see how you did with those harder questions.... You got some things wrong, just to review, I was born in Minneapolis, I went to school at Whitman, and I like Whiskey just slightly more than tacos!' );
-}
-
-if (correct1 && correct2 && correct3 && correct4 && correct5){
-  alert('Wow! You know a lot about me! That\'s kinda creepy!');
-}else if (correct1 || correct2) {
-  alert('Well, at least you got some of the basics right....');
-}else {
-  alert('You didn\'t get much right... Better luck next time bud....');
-}
-
-var userName = prompt('Now that you know who I am, what\'s your name?');
-
-alert('Okay ' + userName + ', now that we know about eachother, let\'s play a game');
+alert('You got ' + correctCounter + ' answers correct!');
 
 //number guessing game
 
