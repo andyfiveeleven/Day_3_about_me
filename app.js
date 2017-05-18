c30ffc42a2736ea80eeece35fa64ba2f30d43349
andyfiveeleven <fouracesguitar@gmail.com>
2017-05-18 13:33:03 -0700

    added functiosn around questions

--- app.js
+++ app.js
@@ -51,67 +51,75 @@ yesNoGame(game1Questions, game1AnswerKey);
 
 var correctAnswers = correctCounter;
 //number guessing game
+function numberGuess(high){
+  var numberGuess;
+  var number = Math.floor(Math.random() * high) + 1;
+  var counter = 1 ;
+  var tries = 3;
 
-var numberGuess;
-var number = Math.floor(Math.random() * 10) + 1;
-var counter = 1 ;
-var tries = 3;
 
-while(numberGuess !== number && counter <= 3){
-  numberGuess = parseInt(prompt(userName + ', I\'m thinking of a number between one and ten, it\'s random, what do you think?'));
+  while(numberGuess !== number && counter <= 3){
+    numberGuess = parseInt(prompt(userName + ', I\'m thinking of a number between one and ' + high + ', it\'s random, what do you think?'));
 
-  if (number < numberGuess) {
-    alert('you guessed too high, try again!');
-    counter++;
-    tries--;
-    alert('You have ' + tries + ' tries left');
-  }else if (number > numberGuess) {
-    alert('you guessed too low, try again!');
-    counter++;
-    tries--;
-    alert('You have ' + tries + ' tries left');
-  }else if(number === isNaN){
-    alert('that\'s not a number dumbass, try again,');
-    counter++;
-    tries--;
-    alert('You have ' + tries + ' tries left');
-  }
-}
-
-console.log('counter: ', counter);
-
-var correct6;
-if (numberGuess !== number){
-  alert('Fail, you ran out of tries');
-}else{
-  alert('Wow! You got it right! And you had ' + tries + ' tries left');
-  correct6 = true;
-  correctAnswers.push(correct6);
-}
-
-//array guessing game
-var hobbies = ['carpentry', 'cocktails', 'cooking', 'camping', 'code'];
-var hobbiesCounter = 0;
-
-while(hobbiesCounter < 6){
-  var hobbiesAnswer = prompt('Can you guess one of my hobbies?').toLowerCase();
-  for(var e=0; e<hobbies.length; e++){
-    console.log(hobbies);
-    if(hobbiesAnswer === hobbies[e]){
-      alert('Well done! I love ' + hobbiesAnswer);
-      hobbiesCounter = 6;
-      var correct7=true;
-      correctAnswers.push(correct7);
-      break;
+    if (number < numberGuess) {
+      alert('you guessed too high, try again!');
+      counter++;
+      tries--;
+      alert('You have ' + tries + ' tries left');
+    }else if (number > numberGuess) {
+      alert('you guessed too low, try again!');
+      counter++;
+      tries--;
+      alert('You have ' + tries + ' tries left');
+    }else if(number === isNaN){
+      alert('that\'s not a number dumbass, try again,');
+      counter++;
+      tries--;
+      alert('You have ' + tries + ' tries left');
     }
   }
-  hobbiesCounter++;
-  if(hobbiesCounter < 6){
-    alert('try again');
+
+  console.log('counter: ', counter);
+
+  var correct6;
+  if (numberGuess !== number){
+    alert('Fail, you ran out of tries');
+  }else{
+    alert('Wow! You got it right! And you had ' + tries + ' tries left');
+    correct6 = true;
+    correctAnswers.push(correct6);
   }
-  console.log(hobbiesAnswer);
-  console.log(hobbiesCounter);
 }
+
+numberGuess(10);
+
+//array guessing game
+function hobbiesGuess(hobbies){
+
+  var hobbiesCounter = 0;
+
+  while(hobbiesCounter < 6){
+    var hobbiesAnswer = prompt('Can you guess one of my hobbies?').toLowerCase();
+    for(var e=0; e<hobbies.length; e++){
+      console.log(hobbies);
+      if(hobbiesAnswer === hobbies[e]){
+        alert('Well done! I love ' + hobbiesAnswer);
+        hobbiesCounter = 6;
+        var correct7=true;
+        correctAnswers.push(correct7);
+        break;
+      }
+    }
+    hobbiesCounter++;
+    if(hobbiesCounter < 6){
+      alert('try again');
+    }
+    console.log(hobbiesAnswer);
+    console.log(hobbiesCounter);
+  }
+}
+
+hobbiesGuess(['carpentry', 'cocktails', 'cooking', 'camping', 'code']);
 
 console.log(correctAnswers);
 
